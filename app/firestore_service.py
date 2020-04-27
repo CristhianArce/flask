8e4ffe7c3084e12c7d3b6c0c70910c4d814a5a9c
cristhian A <cristhianarce30@gmail.com>
2020-04-26 22:05:39 -0500

    Enabling add and delete task from user profile

--- app/firestore_service.py
+++ app/firestore_service.py
@@ -21,3 +21,11 @@ def get_user(user_id):
 def user_put(user_data):
     user_ref = db.collection('users').document(user_data.username)
     user_ref.set({'password': user_data.password})
+
+def put_todo(user_id,description):
+    todos_collection_ref = db.collection('users').document(user_id).collection('todos')
+    todos_collection_ref.add({'description':description})
+
+def delete_todo(user_id,todo_id):
+    todo_ref = db.collection('users').document(user_id).collection('todos').document(todo_id)
+    todo_ref.delete()
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import request, redirect, make_response, render_template, session,flash
+from flask import request, redirect, make_response, render_template, session,flash, url_for
 from app import startApp
-from app.forms import LoginForm
-from app.firestore_service import get_users,get_todos
+from app.forms import LoginForm, TodoForm, DeleteTodoForm
+from app.firestore_service import get_users,get_todos,put_todo,delete_todo
 from flask_login import login_required,current_user
 
 app = startApp()
@@ -24,19 +24,31 @@ def index():
     session['user_ip'] =user_ip
     return response
 
-@app.route("/hello", methods=['GET'])
+@app.route("/hello", methods=['GET','POST'])
 @login_required
 def hello():
     user_ip = session.get("user_ip")
     username = current_user.id
-
+    todo_form = TodoForm()
+    delete_form = DeleteTodoForm()
     context = {
         'user_ip':user_ip,
         'username': username,
-        'todos':get_todos(user_id=username),  
+        'todos':get_todos(user_id=username),
+        'todo_form': todo_form,
+        'delete_form': delete_form,
     }
-
+    if (todo_form.validate_on_submit()):
+        put_todo(user_id=username,description=todo_form.description.data)
+        flash('Task added successfully')
     return render_template("hello.html",**context)
+
+@app.route('/todos/delete/<todo_id>', methods=['POST'])
+def delete(todo_id):
+    user_id = current_user.id
+    delete_todo(user_id=user_id,todo_id=todo_id)
+    return redirect(url_for('hello'))
+
 
 
 
